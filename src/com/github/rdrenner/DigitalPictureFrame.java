44868cd0979a84475feb76519ac44f36c7adbd19
Ray Renner <renner.ray@gmail.com>
2016-12-11 19:52:00 -0500

    1: Create Basic Digital Picture Frame Class and Project Structure
    Task-Url: https://github.com/rdrenner/DigitalPictureFrame/issues/1

---
+++ src/com/github/rdrenner/DigitalPictureFrame.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2016 - Ray Renner
+ */
+package com.github.rdrenner;
+
+/**
+ * @author Ray Renner
+ *
+ */
+public class DigitalPictureFrame
+{
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args)
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+}
