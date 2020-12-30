07d7707cd713f9d1467a1ebf7c73e74ccafd9410
Ray Renner <renner.ray@gmail.com>
2020-12-30 10:52:59 -0500

    Closed #12. Fixed directory and jar names.

--- DigitalPictureFrame-debug.sh
+++ DigitalPictureFrame-debug.sh
@@ -3,4 +3,4 @@
 DIR=`dirname "$BASH_SOURCE[0]"`
 cd "$DIR"
 echo "Current dir: $DIR"
-java -Dlog4j.configurationFile=conf/log4j2-debug.xml -jar DigitalPictureFrame-0.0.1-Alpha.jar &
+java -Dlog4j.configurationFile=conf/log4j2-debug.xml -jar DigitalPictureFrame.jar &
--- DigitalPictureFrame.sh
+++ DigitalPictureFrame.sh
@@ -3,4 +3,4 @@
 DIR=`dirname "$BASH_SOURCE[0]"`
 cd "$DIR"
 echo "Current dir: $DIR"
-java -jar DigitalPictureFrame-0.0.1-Alpha.jar &
+java -jar DigitalPictureFrame.jar &
